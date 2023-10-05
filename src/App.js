b545be6c422c86a87a95abf10baf1b08bc3aaedc
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-10-05 22:49:51 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>REACT TEMPLATE</code>
-        </p>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
